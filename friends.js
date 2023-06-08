e5c79b90c9325594a999a2f538415bf8c0be8a28
Bobby Steckline <rjsturing@gmail.com>
2023-06-07 20:23:33 -0400

    Add sights array

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Madison", "Andrew", "Carly", "Anna", "Chris"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var sights = ["Paris", "Hawaii", "Europe", "Vancouver", "Colorado"];
+sights.push("Mexico");
+console.log(sights);
